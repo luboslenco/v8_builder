a5722a0a0959d92f0e92ef7ec2577f537561a772
luboslenco <lubos.lenco@gmail.com>
2022-12-06 14:53:44 +0100

    Bump v8 version

--- ios.sh
+++ ios.sh
@@ -6,7 +6,7 @@ cd v8
 fetch v8
 echo "target_os = ['ios']" >> .gclient
 cd v8
-git checkout 10.5.218
+git checkout 10.6.194
 gclient sync
 gn gen out/ios --args='ios_enable_code_signing=false ios_deployment_target=14 is_component_build=false is_debug=false target_cpu="arm64" target_os="ios" use_custom_libcxx=false v8_enable_i18n_support=false v8_monolithic=true v8_use_external_startup_data=false v8_enable_snapshot_compression=false symbol_level=0 v8_symbol_level=0 v8_target_cpu="arm64" v8_static_library=true strip_debug_info=true v8_enable_pointer_compression=false v8_enable_31bit_smis_on_64bit_arch=false treat_warnings_as_errors=false dcheck_always_on=false'
 ninja -C out/ios v8_monolith
