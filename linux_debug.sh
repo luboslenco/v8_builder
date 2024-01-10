a1fbe9a53d061b8f83083357a27c10dd620dc0e7
luboslenco <lubos.lenco@gmail.com>
2024-01-10 13:19:33 +0100

    Bump version to 12.2.205

--- linux_debug.sh
+++ linux_debug.sh
@@ -5,7 +5,7 @@ mkdir v8
 cd v8
 fetch v8
 cd v8
-git checkout 12.0.267.14
+git checkout 12.2.205
 gclient sync
 gn gen out/linux --args='is_component_build=false is_debug=true target_cpu="x64" use_custom_libcxx=false v8_enable_i18n_support=false v8_monolithic=true v8_use_external_startup_data=false v8_enable_snapshot_compression=false symbol_level=1 v8_symbol_level=1 v8_static_library=true strip_debug_info=false v8_enable_pointer_compression=false v8_enable_31bit_smis_on_64bit_arch=false dcheck_always_on=false'
 ninja -C out/linux v8_monolith
