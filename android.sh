11f5303d693e30c9a84573c52560d540da278179
luboslenco <lubos.lenco@gmail.com>
2024-01-10 10:39:24 +0100

    Bump version to 12.0.267.14

--- android.sh
+++ android.sh
@@ -6,7 +6,7 @@ cd v8
 fetch v8
 echo "target_os = ['android']" >> .gclient
 cd v8
-git checkout 10.8.169
+git checkout 12.0.267.14
 gclient sync
 gn gen out/android --args='is_component_build=false is_debug=false target_cpu="arm64" target_os="android" use_custom_libcxx=false v8_enable_i18n_support=false v8_monolithic=true v8_use_external_startup_data=false v8_enable_snapshot_compression=false symbol_level=0 v8_symbol_level=0 v8_target_cpu="arm64" v8_static_library=true strip_debug_info=true v8_enable_pointer_compression=false v8_enable_31bit_smis_on_64bit_arch=false treat_warnings_as_errors=false dcheck_always_on=false'
 ninja -C out/android v8_monolith
--- windows.bat
+++ windows.bat
@@ -7,7 +7,7 @@ mkdir v8
 cd v8
 call fetch v8
 cd v8
-call git checkout 10.8.169
+call git checkout 12.0.267.14
 call gclient sync
 call gn gen out\windows --args="is_component_build=false is_debug=false target_cpu=\"x64\" v8_enable_i18n_support=false v8_monolithic=true v8_use_external_startup_data=false v8_enable_snapshot_compression=false symbol_level=0 v8_symbol_level=0 v8_static_library=true strip_debug_info=true v8_enable_pointer_compression=false v8_enable_31bit_smis_on_64bit_arch=false dcheck_always_on=false is_clang=false"
 call ninja -C out\windows v8_monolith
